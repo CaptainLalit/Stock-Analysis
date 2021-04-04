41fa2df6347b9c15376a0b2e45253235db83c58a
lalit5542 <lalit5542@gmail.com>
2021-04-04 23:15:36 +0530

    added cagr

--- main.py
+++ main.py
@@ -8,65 +8,79 @@ now = datetime.now()
 start_year = int(input("Enter start year(>= 2000): "))
 end_year = int(input(f"Enter end year(<= {now.year}): "))
 sip_amount = int(input("Enter sip amount(> 0): "))
+increase_precentage_per_year = float(
+    input("Increase sip amount per year in percentage(>= 0): "))
 symbol = input("Enter index symbol(Ex: NIFTY 50): ")
 
 result = {
     "first_day": {
         "Close": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         },
         "Open": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         }
     },
     "mid_day": {
         "Close": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         },
         "Open": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         }
     },
     "last_day": {
         "Close": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         },
         "Open": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         }
     },
     "second_last_day": {
         "Close": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         },
         "Open": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         }
     },
     "third_last_day": {
         "Close": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         },
         "Open": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         }
     },
     "fourth_last_day": {
         "Close": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         },
         "Open": {
+            "invested": 0,
             "total_units": 0,
             "last_closing_price": 0
         }
@@ -74,8 +88,6 @@ result = {
 }
 
 result_every_day = {}
-
-month_count = 0
 
 print("Skipping current month if current year is selected in analysis")
 
@@ -90,8 +102,6 @@ for y in range(start_year, end_year + 1):
                                end=date(y, m, mr[1]),
                                # expiry_date=get_expiry_date(y, m),
                                index=True)
-
-        month_count += 1
 
         for key, value in result.items():
             investment_day = 0
@@ -110,28 +120,34 @@ for y in range(start_year, end_year + 1):
 
             for investment_time in value.keys():
                 result[key][investment_time]["last_closing_price"] = last_closing_price
+                result[key][investment_time]["invested"] += sip_amount
                 result[key][investment_time]["total_units"] += sip_amount / \
                     nse_data.iloc[investment_day][investment_time]
 
-        for investment_day in range(1, len(nse_data) + 1):
-            if not bool(result_every_day.get(f"day_{investment_day}", None)):
-                result_every_day[f"day_{investment_day}"] = {
-                    "Close": {
-                        "total_units": 0,
-                        "last_closing_price": 0
-                    },
-                    "Open": {
-                        "total_units": 0,
-                        "last_closing_price": 0
-                    }
-                }
+        # for investment_day in range(1, len(nse_data) + 1):
+        #     if not bool(result_every_day.get(f"day_{investment_day}", None)):
+        #         result_every_day[f"day_{investment_day}"] = {
+        #             "Close": {
+        #                 "invested": 0,
+        #                 "total_units": 0,
+        #                 "last_closing_price": 0
+        #             },
+        #             "Open": {
+        #                 "invested": 0,
+        #                 "total_units": 0,
+        #                 "last_closing_price": 0
+        #             }
+        #         }
 
-            last_closing_price = nse_data.iloc[investment_day - 1]["Close"]
+        #     last_closing_price = nse_data.iloc[investment_day - 1]["Close"]
 
-            for investment_time in result_every_day[f"day_{investment_day}"].keys():
-                result_every_day[f"day_{investment_day}"][investment_time]["last_closing_price"] = last_closing_price
-                result_every_day[f"day_{investment_day}"][investment_time]["total_units"] += sip_amount / \
-                    nse_data.iloc[investment_day - 1][investment_time]
+        #     for investment_time in result_every_day[f"day_{investment_day}"].keys():
+        #         result_every_day[f"day_{investment_day}"][investment_time]["last_closing_price"] = last_closing_price
+        #         result_every_day[f"day_{investment_day}"][investment_time]["total_units"] += sip_amount / \
+        #             nse_data.iloc[investment_day - 1][investment_time]
+
+        # increment the sip amount
+        sip_amount += int(sip_amount * (increase_precentage_per_year / 100))
 
     print("=" * 50)
     print(f"YEAR {y}")
@@ -139,8 +155,9 @@ for y in range(start_year, end_year + 1):
         print("*" * 21, "  ", key, "  ", "*" * 21)
         for investment_time in value.keys():
             print("-" * 21, "  ", investment_time, "  ", "-" * 21)
+            print(f"Current SIP amount: {sip_amount}")
             print(
-                f"Total amount invested till year {y}: {month_count * sip_amount}")
+                f'Total amount invested till year {y}: {result[key][investment_time]["invested"]}')
             print(
                 f'Total value at the end of the year {y}: {int(result[key][investment_time]["last_closing_price"] * result[key][investment_time]["total_units"])}')
             print(
@@ -155,13 +172,20 @@ print(f"Final Result: ")
 for key, value in result.items():
     print("*" * 21, "  ", key, "  ", "*" * 21)
     for investment_time in value.keys():
+        amount_invested = result[key][investment_time]["invested"]
+        current_value = int(result[key][investment_time]["last_closing_price"]
+                            * result[key][investment_time]["total_units"])
         print("-" * 21, "  ", investment_time, "  ", "-" * 21)
         print(
-            f"Total amount invested: {month_count * sip_amount}")
+            f"Total amount invested: {amount_invested}")
         print(
-            f'Total value: {int(result[key][investment_time]["last_closing_price"] * result[key][investment_time]["total_units"])}')
+            f'Total value: {current_value}')
         print(
             f'Total units bought via sip of {sip_amount}: {result[key][investment_time]["total_units"]}')
+        print(
+            f'Absolute returns: {(current_value - amount_invested) / amount_invested * 100}')
+        print(
+            f'SIP CAGR on final invested amount: {((current_value / amount_invested) ** (1 / (end_year - start_year)) - 1) * 100}')
         print("*" * 50)
 print("=" * 50)
 print("\n\n")
@@ -171,18 +195,18 @@ print(result)
 print("=" * 50)
 print("\n\n")
 
-print("=" * 50)
-print(f"Final Result of investing in any particular day of month: ")
-for key, value in result_every_day.items():
-    print(
-        key,
-        " -> ",
-        f'{int(value["Open"]["last_closing_price"]* value["Open"]["total_units"])} / {int(value["Open"]["total_units"])}',
-        "  ",
-        f'{int(value["Close"]["last_closing_price"] * value["Close"]["total_units"])} / {int(value["Open"]["total_units"])}'
-    )
-print("\n\n")
-print("=" * 50)
-print("Raw result")
-print(result_every_day)
-print("=" * 50)
+# print("=" * 50)
+# print(f"Final Result of investing in any particular day of month: ")
+# for key, value in result_every_day.items():
+#     print(
+#         key,
+#         " -> ",
+#         f'{int(value["Open"]["last_closing_price"]* value["Open"]["total_units"])} / {int(value["Open"]["total_units"])}',
+#         "  ",
+#         f'{int(value["Close"]["last_closing_price"] * value["Close"]["total_units"])} / {int(value["Open"]["total_units"])}'
+#     )
+# print("\n\n")
+# print("=" * 50)
+# print("Raw result")
+# print(result_every_day)
+# print("=" * 50)
